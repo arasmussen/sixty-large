215a2ae5a2b288f7ec5a659dd4205025035b1dcc
Andrew Rasmussen <andrew@venz.io>
2014-02-18 23:26:05 -0800

    hook up drop down box

--- root/js/script.js
+++ root/js/script.js
@@ -1,19 +1,64 @@
 $(function() {
+  // seed the random number generator
+  // KEEP THIS AT THE TOP
+  rng_seed_time();
 
-  $( document ).ready(function() {
-      $('#coin-selector').on('click', function () {
-        $('#currency-list').toggleClass('active');
-      });
+  $('#coin-selector').on('click', function() {
+    $('#currency-list').toggleClass('active');
   });
 
-  // seed the random number generator
-  rng_seed_time();
+  $('#coin-selector li').on('click', function(target) {
+    // remove selected from current one
+    $('#coin-selector li.selected').removeClass('selected');
+
+    // add it to the new one
+    var li = $(event.target);
+    li.addClass('selected');
+
+    var coinName = li.html();
+    var coinAbbrev = coinToAbbrevMap[coinName];
+
+    // update the label
+    $('#coin-selector #selected-coin-label').html(coinName);
+
+    // update key gen stuff
+    updatePrefix(coinAbbrev);
+    generateWalletFromKey();
+  });
 
   var generateButton = $('#generate-button');
   generateButton.bind('click', generateRandomWallet);
 
   var secretKey = $('#secret');
   secretKey.bind('input', generateWalletFromKey);
+
+  var prefixes = {
+    btc: {
+      private: 128,
+      public: 0
+    },
+    ltc: {
+      private: 176,
+      public: 48
+    }
+  };
+
+  var coinToAbbrevMap = {
+    Bitcoin: 'btc',
+    Litecoin: 'ltc'
+  };
+
+  function updatePrefix(coinAbbrev) {
+    if (!prefixes[coinAbbrev]) {
+      console.error("Coin abbreviation \"" + coinAbbrev + "\" doesn't exist");
+      return;
+    }
+    prefix.private = prefixes[coinAbbrev].private,
+    prefix.public = prefixes[coinAbbrev].public
+  }
+
+  var prefix = {};
+  updatePrefix('btc'); // start with btc by default
 
   function isValidHex(str) {
     return !/[^0123456789abcdef]+/i.test(str);
@@ -58,7 +103,7 @@ $(function() {
   }
 
   function privateKeyFromSecretKey(secretKey) {
-    var extended = [128].concat(secretKey);
+    var extended = [prefix.private].concat(secretKey);
     var hashed = Crypto.SHA256(Crypto.SHA256(extended, {asBytes: true}), {asBytes: true});
     var checksum = hashed.slice(0, 4);
     var fullExtended = extended.concat(checksum);
@@ -75,7 +120,7 @@ $(function() {
     var publicKeyBytes = integerToBytes(x, 32);
     publicKeyBytes = publicKeyBytes.concat(integerToBytes(y, 32));
     publicKeyBytes.unshift(0x04);
-    var hashedOnce = [0].concat(Bitcoin.Util.sha256ripe160(publicKeyBytes));
+    var hashedOnce = [prefix.public].concat(Bitcoin.Util.sha256ripe160(publicKeyBytes));
     var hashedThrice = Crypto.SHA256(Crypto.SHA256(hashedOnce, {asBytes: true}), {asBytes: true});
     var checksum = hashedThrice.slice(0, 4);
     var fullExtended = hashedOnce.concat(checksum);
