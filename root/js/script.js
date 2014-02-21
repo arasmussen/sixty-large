75ca9c31844ab042312253c2c828ef8f6aa32715
Andrew Rasmussen <andrew@venz.io>
2014-02-20 20:35:32 -0800

    add codes for 9 more coins

--- root/js/script.js
+++ root/js/script.js
@@ -33,7 +33,23 @@ $(function() {
   secretKey.bind('input', generateWalletFromKey);
 
   var prefixes = {
+    bbq: {
+      private: 213,
+      public: 85
+    },
+    btbr: {
+      private: 153,
+      public: 25
+    },
     btc: {
+      private: 128,
+      public: 0
+    },
+    byte: {
+      private: 128,
+      public: 18
+    },
+    dev: {
       private: 128,
       public: 0
     },
@@ -41,21 +57,50 @@ $(function() {
       private: 158,
       public: 30
     },
+    feather: {
+      private: 142,
+      public: 14
+    },
+    frei: {
+      private: 128,
+      public: 0
+    },
     ltc: {
       private: 176,
       public: 48
     },
+    name: {
+      private: 180,
+      public: 52
+    },
+    nova: {
+      private: 136,
+      public: 8
+    },
     peer: {
       private: 183,
       public: 55
+    },
+    terra: {
+      private: 128,
+      public: 0
     }
   };
 
   var coinToAbbrevMap = {
+    Bbqcoin: 'bbq',
+    Bitbar: 'btbr',
     Bitcoin: 'btc',
+    Bytecoin: 'byte',
+    Devcoin: 'dev',
     Dogecoin: 'doge',
+    Feathercoin: 'feather',
+    Freicoin: 'frei',
     Litecoin: 'ltc',
-    Peercoin: 'peer'
+    Namecoin: 'name',
+    Novacoin: 'nova',
+    Peercoin: 'peer',
+    Terracoin: 'terra'
   };
 
   function updatePrefix(coinAbbrev) {
