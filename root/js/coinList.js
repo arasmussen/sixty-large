aa4de4e642c6dc101bb5f19f19ae0f624847efab
Andrew Rasmussen <andrew@venz.io>
2014-02-25 21:50:56 -0800

    wallet-container

--- root/js/coinList.js
+++ root/js/coinList.js
@@ -35,18 +35,20 @@ define([
         this.coinNames.forEach(function(coinName) {
           var li = '' +
             '<li id="' + coinName + '-wallet">' +
-              '<div class="left">' +
-                '<h2>' + coinName + '</h2>' +
-              '</div>' +
-              '<div class="right">' +
-                '<div class="keys">' +
-                  '<div class="public">' +
-                    '<span />' +
-                    '<h3>PUBLIC ADDRESS</h3>' +
-                  '</div>' +
-                  '<div class="private">' +
-                    '<span />' +
-                    '<h3>PRIVATE ADDRESS</h3>' +
+              '<div class="wallet-container">' +
+                '<div class="left">' +
+                  '<h2>' + coinName + '</h2>' +
+                '</div>' +
+                '<div class="right">' +
+                  '<div class="keys">' +
+                    '<div class="public">' +
+                      '<span />' +
+                      '<h3>PUBLIC ADDRESS</h3>' +
+                    '</div>' +
+                    '<div class="private">' +
+                      '<span />' +
+                      '<h3>PRIVATE ADDRESS</h3>' +
+                    '</div>' +
                   '</div>' +
                 '</div>' +
               '</div>' +
